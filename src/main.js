15e11d37507f2dafa7110f15438e3a266be27720
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-03-09 17:46:47 +0200

    added singup and signin with firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import * as firebase from 'firebase'
 import App from './App'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
-import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -15,5 +16,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCkemNk2Lkb2JILhTYqfae9o_gRhC3i2vY',
+      authDomain: 'vue-meetup-7b6ac.firebaseapp.com',
+      databaseURL: 'https://vue-meetup-7b6ac.firebaseio.com',
+      projectId: 'vue-meetup-7b6ac',
+      storageBucket: 'vue-meetup-7b6ac.appspot.com'
+    })
+  }
 })
